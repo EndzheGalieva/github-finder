83c21c798d7a6a12ee1248cf478eacce6affa8d7
Endzhe Galieva <endzhe91@gmail.com>
2021-05-06 16:40:50 +0300

    add new button

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -45,6 +45,29 @@ export class User extends Component {
        ) : (
          <i className="fas fa-times-circle text-danger" />
          )}
+         <div className="card grid-2">
+           <div className="all-center">
+             <img
+               src={avatar_url}
+               className="round-img"
+               alt=""
+               style={{width: '150px'}}
+             />
+             <h1>{name}</h1>
+             <p>Location: {location}</p>
+           </div>
+           <div>
+             {bio && (
+               <Fragment>
+                 <h3>Bio</h3>
+                 <p>{bio}</p>
+               </Fragment>
+             )}
+             <a href={html_url} className="btn btn-dark my-1">
+               Visit Github Profile
+             </a>
+           </div>
+         </div>
        </Fragment>
     );
   }
